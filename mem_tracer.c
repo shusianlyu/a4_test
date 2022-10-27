91657b5d14ec2b36b74dbf7c91f2fdb18fc8c2b0
Shu Sian Lyu <shusianlyu@gmail.com>
2022-10-27 14:13:58 -0700

    Final version

--- mem_tracer.c
+++ mem_tracer.c
@@ -1,3 +1,11 @@
+/**
+ * file: mem_tracer.c
+ * author: Shu Sian (Jessie) Lyu
+ * description: program that store the
+ * command lines in a dynamically allocated array of type char **
+ * and trace the memory usage.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,18 +25,28 @@ struct TRACE_NODE_STRUCT {
 typedef struct TRACE_NODE_STRUCT TRACE_NODE;
 static TRACE_NODE* TRACE_TOP = NULL;       // ptr to the top of the stack
 
+/**
+ * CMD_NODE_STRUCT is a linked list of char array
+ * CMD_TOP is the head of the list.
+**/
 struct CMD_NODE_STRUCT {
-    char cmd[100];                       // pointer to each line
-    int line;                        // lineCount
-    struct CMD_NODE_STRUCT* next;    // ptr to next cmd
+    char cmd[100];                       // char array to store each command
+    int line;                            // lineIndex of the command
+    struct CMD_NODE_STRUCT* next;        // ptr to next cmd
 };
 typedef struct CMD_NODE_STRUCT CMD_NODE;
-static CMD_NODE* CMD_TOP = NULL;       // ptr to the top of the stack
+static CMD_NODE* CMD_TOP = NULL;         // ptr to the top of the list
 
-/**
- *
- * @param p
- */
+/* function PUSH_TRACE */
+/*
+ * The purpose of this stack is to trace the sequence of function calls,
+ * just like the stack in your computer would do.
+ * The "global" string denotes the start of the function call trace.
+ * The char *p parameter is the name of the new function that is added to the call
+trace.
+ * See the examples of calling PUSH_TRACE and POP_TRACE below
+ * in the main, make_extend_array, add_column functions.
+**/
 void PUSH_TRACE(char* p)          // push p on the stack
 {
     TRACE_NODE* tnode;
@@ -57,129 +75,84 @@ void PUSH_TRACE(char* p)          // push p on the stack
     tnode->functionid = p;
     tnode->next = TRACE_TOP;  // insert tnode as the first in the list
     TRACE_TOP = tnode;          // point TRACE_TOP to the first node
-} /*end PUSH_TRACE*/
+} /* end PUSH_TRACE */
 
+/* function POP_TRACE */
+/* Pop a function call from the stack */
 void POP_TRACE()    // remove the op of the stack
 {
     TRACE_NODE* tnode;
     tnode = TRACE_TOP;
     TRACE_TOP = tnode->next;
     free(tnode);
-} /*end POP_TRACE*/
+} /* end POP_TRACE */
 
+/* function PRINT_TRACE prints out the sequence of function
+ * calls that are on the stack at this instance */
+/* For example: funcC:funcB:funcA:global */
 char* PRINT_TRACE()
 {
-    int depth = 50; // A max of 50 levels in the stack will be combined in a string for printing out.
+    int depth = 50;              // A max of 50 levels in the stack will be combined in a string for printing out.
     int i, length, j;
     TRACE_NODE* tnode;
     static char buf[100];
     if (TRACE_TOP == NULL) {     // stack not initialized yet, so we are
-        strcpy(buf,"global");   // still in the `global' area
+        strcpy(buf,"global");    // still in the `global' area
         return buf;
     }
     /* peek at the depth(50) top entries on the stack, but do not
        go over 100 chars and do not go over the bottom of the
        stack */
     sprintf(buf,"%s",TRACE_TOP->functionid);
-    length = strlen(buf);                  // length of the string so far
+    length = strlen(buf);                     // length of the string so far
     for(i = 1, tnode = TRACE_TOP->next; tnode!=NULL && i < depth; i++, tnode = tnode->next) {
-        j = strlen(tnode->functionid);             // length of what we want to add
+        j = strlen(tnode->functionid);        // length of what we want to add
         if (length + j + 1 < 100) {              // total length is ok
             sprintf(buf+length, ":%s", tnode->functionid);
             length += j + 1;
-        }else                                // it would be too long
+        }else                                    // it would be too long
             break;
     }
     return buf;
 } /*end PRINT_TRACE*/
 
-// For instance, example of print out:
-// "File tracemem.c, line X, function F reallocated the memory segment at address A to a new size S"
-// Information about the function F should be printed by printing the stack (use PRINT_TRACE)
+/** function REALLOC calls realloc and
+ * it trace the memory consumption.
+ */
 void* REALLOC(void* p, int t, char* file, int line)
 {
     p = realloc(p,t);
     printf("File %s, line %d, function=%s reallocated the memory segment at address %p to a new size %d\n", file, line, PRINT_TRACE(), p, t);
     return p;
-}
+} /* end REALLOC */
 
-// For instance, example of print out:
-// "File tracemem.c, line X, function F allocated new memory segment at address A to size S"
-// Information about the function F should be printed by printing the stack (use PRINT_TRACE)
+/** function MALLOC calls malloc and
+ * it trace the memory consumption.
+ */
 void* MALLOC(int t, char* file, int line)
 {
     void* p;
     p = malloc(t);
     printf("File %s, line %d, function=%s allocated new memory segment at address %p to size %d\n", file, line, PRINT_TRACE(), p, t);
     return p;
-}
+} /* end MALLOC */
 
-// For instance, example of print out:
-// "File tracemem.c, line X, function F deallocated the memory segment at address A"
-// Information about the function F should be printed by printing the stack (use PRINT_TRACE)
+/** function FREE calls free and
+ * it trace the memory consumption.
+ */
 void FREE(void* p, char* file, int line)
 {
     printf("File %s, line %d, function=%s deallocated the memory segment at address %p\n", file, line, PRINT_TRACE(), p);
     free(p);
-}
+} /* end FREE */
 
 #define realloc(a,b) REALLOC(a,b,__FILE__,__LINE__)
 #define malloc(a) MALLOC(a,__FILE__,__LINE__)
 #define free(a) FREE(a,__FILE__,__LINE__)
 
-// -----------------------------------------
-// function add_column will add an extra column to a 2d array of ints.
-// This function is intended to demonstrate how memory usage tracing of realloc is done
-// Returns the number of new columns (updated)
-int add_column(int** array,int rows,int columns)
-{
-    PUSH_TRACE("add_column");
-    int i;
-    for(i=0; i<rows; i++) {
-        array[i]=(int*) realloc(array[i],sizeof(int)*(columns+1));
-        array[i][columns]=10*i+columns;
-    }//for
-    POP_TRACE();
-    return (columns+1);
-} // end add_column
-
-// ------------------------------------------
-// function make_extend_array
-// Example of how the memory trace is done
-// This function is intended to demonstrate how memory usage tracing of malloc and free is done
-void make_extend_array()
-{
-    PUSH_TRACE("make_extend_array");
-    int i, j;
-    int **array;
-    int ROW = 4;
-    int COL = 3;
-    //make array
-    array = (int**) malloc(sizeof(int*) * 4);  // 4 rows
-    for(i = 0; i < ROW; i++) {
-        array[i] = (int*) malloc(sizeof(int) * 3);  // 3 columns
-        for(j = 0; j < COL; j++)
-            array[i][j] = 10*i+j;
-    } // end of for
-
-    //display array
-    for(i = 0; i < ROW; i++)
-        for(j = 0; j < COL; j++)
-            printf("array[%d][%d]=%d\n", i, j, array[i][j]);
-    // and a new column
-    int NEWCOL = add_column(array,ROW,COL);
-    // now display the array again
-    for(i = 0; i < ROW; i++)
-        for(j = 0; j < NEWCOL; j++)
-            printf("array[%d][%d]=%d\n", i, j, array[i][j]);
-    //now deallocate it
-    for(i = 0; i < ROW; i++)
-        free((void*)array[i]);
-    free((void*)array);
-    POP_TRACE();
-    return;
-} //end make_extend_array
-
+/** function insert_node inserts each command line
+ * with their command index into a linked list
+ */
 void insert_node(char* buf, int cmdIndex)      // insert command to the list with line count
 {
     PUSH_TRACE("insert_node");
@@ -219,8 +192,11 @@ void insert_node(char* buf, int cmdIndex)      // insert command to the list wit
     }
     POP_TRACE();
     return;
-} /*end insert_node */
+} /* end insert_node */
 
+/** function print_nodes display each command line
+ * with their command index in the linked list
+ */
 void print_nodes(){
 
     PUSH_TRACE("print_nodes");
@@ -237,8 +213,11 @@ void print_nodes(){
     }
     POP_TRACE();
     return;
-}
+} /* end print_nodes */
 
+/** function free_list call free
+ * to free each node in the linked list
+ */
 void free_list(CMD_NODE *head){
     PUSH_TRACE("free_list");
     CMD_NODE *cur;
@@ -249,25 +228,24 @@ void free_list(CMD_NODE *head){
     }
     POP_TRACE();
     return;
-}
+} /* end free_list */
 
 int main() {
-
     PUSH_TRACE("main");
-    char *cmd;              // ?
-    char **cmdsPtr;         // dynamically allocated array of type char **
-    int numOfCmds = 10;           // Start with 10 commands
+    char *cmd;                // char array
+    char **cmdsPtr;           // dynamically allocated array of type char **
+    int numOfCmds = 10;       // Start with 10 commands
     size_t nbytes = 100;
-    int cmdIndex = 0;       // holds line index
+    int cmdIndex = 0;         // holds line index
 
     int outFile = open("memtrace.out",  O_RDWR | O_CREAT | O_APPEND, 0777);
     if (outFile < 0){
         printf("Error opening file\n");
     }
-    dup2(outFile, 1);       // redirecting stdout to memtrace.out file
+    dup2(outFile, 1);         // redirecting stdout to memtrace.out file
 
     cmd = (char *) malloc(nbytes + 1);
-    cmdsPtr = malloc(sizeof(char*) * numOfCmds);   // an initial size of 10 char * pointers.
+    cmdsPtr = malloc(sizeof(char*) * numOfCmds);    // an initial size of 10 char * pointers.
     if (cmdsPtr == NULL){
         perror("Unable to allocate buffer");
         exit(1);
@@ -280,21 +258,19 @@ int main() {
 
         if(cmdIndex == numOfCmds){          // check if the initial size turns out not to be big enough
             numOfCmds *= 2;                 // expand to double the current size
-            cmdsPtr = realloc(cmdsPtr, sizeof(char*) * numOfCmds); // re allocate memory.
+            cmdsPtr = realloc(cmdsPtr, sizeof(char*) * numOfCmds); // reallocate memory.
         }
-        cmdsPtr[cmdIndex++] = cmd;
-        insert_node(cmd, cmdIndex);
+        cmdsPtr[cmdIndex++] = cmd;          // store into dynamically allocated array
+        insert_node(cmd, cmdIndex);    // insert each command into linked list
     }
 
-    print_nodes();
-    //make_extend_array();
-    free_list(CMD_TOP);
+    print_nodes();                     // display nodes in the list
 
-    free(cmd);
-    free(cmdsPtr);
+    free_list(CMD_TOP);          // free linked list memory
+    free(cmd);                         // free cmd memory
+    free(cmdsPtr);                     // free cmdsPtr memory
 
     POP_TRACE();
-    
-    return(0);
 
+    return(0);
 }
